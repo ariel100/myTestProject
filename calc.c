155e475efd83153add31efbace7cbb50a7e9f773
Ariel Levy <ariel.levi@motorolasolutions.com>
2018-02-18 16:02:47 +0200

    first commit

---
+++ calc.c
@@ -0,0 +1,12 @@
+int main()
+{
+   int val;
+   
+   printf("calc version 1.0\n");
+   
+   val = calc(100);
+   
+   printf("cvalue is %d\n", val);
+   
+   return 0;
+}
---
+++ util.c
@@ -0,0 +1,4 @@
+int calc(int val)
+{
+  return val*10;
+}
